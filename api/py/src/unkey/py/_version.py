92eac47978b6ebac8a590cc99ab616f6c1279b29
chronark <dev@chronark.com>
2025-07-31 10:18:29 +0200

    fix: idk man I'm lost

--- api/py/src/unkey/py/_version.py
+++ api/py/src/unkey/py/_version.py
@@ -3,10 +3,10 @@
 import importlib.metadata
 
 __title__: str = "unkey.py"
-__version__: str = "0.2.3"
+__version__: str = "2.0.0"
 __openapi_doc_version__: str = "2.0.0"
 __gen_version__: str = "2.668.4"
-__user_agent__: str = "speakeasy-sdk/python 0.2.3 2.668.4 2.0.0 unkey.py"
+__user_agent__: str = "speakeasy-sdk/python 2.0.0 2.668.4 2.0.0 unkey.py"
 
 try:
     if __package__ is not None:
